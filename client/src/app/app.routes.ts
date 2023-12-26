3f13b1c3a36294ee40ee6dafb662212c3d517c75
Isaac177 <isaacjogues177@gmail.com>
2023-12-26 16:09:36 +0600

    fix base url2

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,15 +1,12 @@
-
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { EmployeeCreateComponent } from './employee-create/employee-create.component';
-import {EditEmployeeComponent} from "./edit-employee/edit-employee.component";
+import { EditEmployeeComponent } from "./edit-employee/edit-employee.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/employees', pathMatch: 'full' },
-  { path: 'employees', component: EmployeesListComponent },
+  { path: '', component: EmployeesListComponent, pathMatch: 'full' },
   { path: 'employees/:id', component: EmployeeDetailComponent },
   { path: 'create-employee', component: EmployeeCreateComponent },
   { path: 'edit-employee/:id', component: EditEmployeeComponent },
-
 ];
